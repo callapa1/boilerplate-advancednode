e2ed59d2fa89c35a9d83cf9933b2ff97569c26bf
Cristian Lopez <lopezcallapa@gmail.com>
2025-03-09 14:18:36 +0000

    auth

--- auth.js
+++ auth.js
@@ -1,42 +1,54 @@
+require('dotenv').config();
+const GithubStrategy = require('passport-github').Strategy;
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const { ObjectID } = require('mongodb');
+
 module.exports = function (app, myDataBase) {
-    require('dotenv').config();
-    const passport = require('passport');
-    const GithubStrategy = require('passport-github').Strategy;
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+    passport.deserializeUser((id, done) => {
+        myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+            done(null, doc);
+        })
+    });
+
 
     passport.use(new GithubStrategy({
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         callbackURL: 'http://cristian.testapp'
     },
-    function (accessToken, refreshToken, profile, cb) {
-        console.log(profile);
-        myDataBase.findOneAndUpdate(
-            {id: profile.id},
-            {
-                $setOnInsert: {
-                    id: profile.id,
-                    username: profile.username,
-                    name: profile.displayName || 'John Doe',
-                    photo: profile.photos[0].value || '',
-                    email: Array.isArray(profile.emails)
-                    ? profile.emails[0].value
-                    : 'Np public email',
-                    provider: profile.provider || ''
+        function (accessToken, refreshToken, profile, cb) {
+            console.log(profile);
+            myDataBase.findOneAndUpdate(
+                { id: profile.id },
+                {
+                    $setOnInsert: {
+                        id: profile.id,
+                        username: profile.username,
+                        name: profile.displayName || 'John Doe',
+                        photo: profile.photos[0].value || '',
+                        email: Array.isArray(profile.emails)
+                            ? profile.emails[0].value
+                            : 'Np public email',
+                        provider: profile.provider || ''
+                    },
+                    $set: {
+                        last_login: new Date()
+                    },
+                    $inc: {
+                        login_count: 1
+                    }
                 },
-                $set: {
-                    last_login: new Date()
-                },
-                $inc: {
-                    login_count: 1
+                { upsert: true, new: true },
+                (err, doc) => {
+                    return cb(null, doc.value)
                 }
-            },
-            { upsert: true, new: true },
-            (err, doc) => {
-                return cb(null, doc.value)
-            }
-        )
-    }
-));
+            )
+        }
+    ));
 
     passport.use(new LocalStrategy((username, password, done) => {
         myDataBase.findOne({ username: username }, (err, user) => {
@@ -47,13 +59,4 @@ module.exports = function (app, myDataBase) {
             return done(null, user);
         })
     }))
-
-    passport.serializeUser((user, done) => {
-        done(null, user._id);
-    });
-    passport.deserializeUser((id, done) => {
-        myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-            done(null, doc);
-        })
-    });
 }
