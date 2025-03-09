5fa6c6011d36ccb11b979fe92c0fe70513480cdd
Cristian Lopez <lopezcallapa@gmail.com>
2025-03-09 13:27:54 +0000

    loggin in out and registering

--- server.js
+++ server.js
@@ -35,24 +35,55 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
-  app.route('/login').post(passport.authenticate('local', {failureRedirect: '/'}), (req, res) => {
-    res.redirect('/profile')
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
   });
 
-  app.route('/profile').get(ensureAuthenticated, (req,res) => {
-    res.render('profile', {username: req.user.username});
+  app.route('/profile').get(
+    ensureAuthenticated,
+    (req, res) => {
+      res.render('profile', {username: req.user.username});
+    });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
   });
 
   app
-    .route('/logout')
-    .get((req, res) => {
-      req.logout();
-      res.redirect('/');
-    });
+    .route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({username: req.body.username}, (err, user) => {
+        if(err) {
+          next(err);
+        } else if(user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+        }
+      })
+    },
+    passport.authenticate('local', {failureRedirect: '/'}),
+      (req, res, next) => {
+        res.redirect('/profile');
+      }
+  );
 
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
